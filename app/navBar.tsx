caf996cbc0308b982c4ea291cd40371362911f10
HuyN <huyn.dev.2105@gmail.com>
2024-01-23 21:00:08 +0700

    update nav

--- app/navBar.tsx
+++ app/navBar.tsx
@@ -1,6 +1,5 @@
 'use client';
 import React from 'react';
-import { Dropdown, DropdownTrigger } from '@nextui-org/dropdown';
 import Link from 'next/link';
 
 const NavBar = ({ currentPage }) => {
@@ -21,61 +20,57 @@ const NavBar = ({ currentPage }) => {
 
 	return (
 		<>
-			<div className='navbar bg-base-100 shadow'>
-				<div className='flex-none dropdown dropdown-end'>
-					<Dropdown>
-						<DropdownTrigger>
-							<button tabIndex={0} className='btn btn-square btn-ghost'>
-								<svg
-									xmlns='http://www.w3.org/2000/svg'
-									fill='none'
-									viewBox='0 0 24 24'
-									className='inline-block w-5 h-5 stroke-current'
-								>
-									<path
-										strokeLinecap='round'
-										strokeLinejoin='round'
-										strokeWidth='2'
-										d='M4 6h16M4 12h16M4 18h16'
-									></path>
-								</svg>
-							</button>
-						</DropdownTrigger>
-						<ul className='menu dropdown-content z-[1] p-2 shadow bg-base-100 rounded-box w-52 mt-4'>
-							{pages.map((page) => (
-								<li key={page.name}>
-									<Link
-										href={page.pageLink}
-										className={page.name == currentPage ? 'text-info' : ''}
-									>
-										{page.name}
-									</Link>
-								</li>
-							))}
-						</ul>
-					</Dropdown>
+			<div className='navbar bg-base-100'>
+				<div className='flex-1 navbar-start'>
+					<a className='btn btn-ghost text-xl'>yourTodo</a>
 				</div>
-				<div className='flex-1'>
-					<Link href='/' className='btn btn-ghost text-xl'>
-						YourTodo
-					</Link>
-				</div>
-				<div className='flex-none'>
-					<button className='btn btn-square btn-ghost'>
-						<svg
-							xmlns='http://www.w3.org/2000/svg'
-							fill='none'
-							viewBox='0 0 24 24'
-							className='inline-block w-5 h-5 stroke-current'
+				<div
+					role='tablist'
+					className='tabs tabs-lifted navbar-center w-20 translate-y-0.25'
+				>
+					{pages.map((page) => (
+						<Link
+							role='tab'
+							className={page.name == currentPage ? 'tab tab-active' : 'tab'}
+							key={page.name}
+							href={page.pageLink}
 						>
-							<path
-								strokeLinecap='round'
-								strokeLinejoin='round'
-								strokeWidth='2'
-								d='M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z'
-							></path>
-						</svg>
-					</button>
+							{page.name}
+						</Link>
+					))}
+				</div>
+				<div className='flex-none gap-2 navbar-end'>
+					<div className='dropdown dropdown-end'>
+						<div
+							tabIndex={0}
+							role='button'
+							className='btn btn-ghost btn-circle avatar'
+						>
+							<div className='w-10 rounded-full'>
+								<img
+									alt='Tailwind CSS Navbar component'
+									src='https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg'
+								/>
+							</div>
+						</div>
+						<ul
+							tabIndex={0}
+							className='mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52'
+						>
+							<li>
+								<a className='justify-between'>
+									Profile
+									<span className='badge'>New</span>
+								</a>
+							</li>
+							<li>
+								<a>Settings</a>
+							</li>
+							<li>
+								<a className='text-warning'>Logout</a>
+							</li>
+						</ul>
+					</div>
 				</div>
 			</div>
 		</>
