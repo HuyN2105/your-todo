8045adb96edb9116f816fc81af2526cc4aa32fb6
HuyN <huyn.dev.2105@gmail.com>
2024-01-24 18:53:12 +0700

    update addTodo

--- app/todo/AddTodo.tsx
+++ app/todo/AddTodo.tsx
@@ -4,11 +4,11 @@ import React from 'react';
 function AddTodo() {
 	return (
 		<>
-			<form className='fixed flex top-full -translate-y-14 w-full'>
+			<form className='fixed flex justify-center top-full -translate-y-14 -translate-x-4 w-full'>
 				<input
 					type='text'
 					placeholder='Add Todo'
-					className='input input-bordered input-primary w-9/12 mr-px'
+					className='input input-bordered input-primary w-9/12 mr-1'
 				/>
 				<button className='btn btn-outline btn-info float-end'>Add</button>
 			</form>
