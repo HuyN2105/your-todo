1f7537e46dbcd06ba105ea67cd1c83bff9a61784
HuyN <huyn.dev.2105@gmail.com>
2024-01-31 15:36:49 +0700

    auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -58,7 +58,7 @@ const authOptions: AuthOptions = {
 	session: {
 		strategy: 'jwt',
 	},
-	secret: process.env.NEXTAUTH_SECRET,
+	secret: 'HuyDepTrai',
 };
 
 const handler = NextAuth(authOptions);
