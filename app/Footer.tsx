c0b8c12f30123999b86db19f92213c304808476f
HuyN <huyn.dev.2105@gmail.com>
2024-02-02 02:13:28 +0700

    footer

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -39,7 +39,7 @@ function Footer() {
 				</div>
 			</nav>
 			<aside>
-				<p>Copyright © 2024 - All right reserved by ACME Industries Ltd</p>
+				<p>Copyright © 2024 - Nguyễn Thanh Huy </p>
 			</aside>
 		</footer>
 	);
